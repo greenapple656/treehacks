6cf88c18f8ab2b851a2323d665da7d1869dfa520
Amy Wang <greenapple656@gmail.com>
2017-02-18 05:10:24 -0700

    Update Link.java

--- Link.java
+++ Link.java
@@ -1,16 +1,29 @@
 package treehacks;
 
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class Link {
-	public Scene scene;
-	public boolean available;
-	public Rectangle hotspot;
-	
-	public Link(Scene scene, boolean available, Rectangle hotspot)
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Link extends JButton {	
+	public Link(String name, HashMap<String, Scene> ht, JFrame root, Scene curr, Rectangle rect)
 	{
-		this.scene = scene;
-		this.available = available;
-		this.hotspot = hotspot;
+		setSize(rect.width, rect.height);
+		setLocation(rect.x, rect.y);
+		//gross hack to prevent the ActionListener from erroring
+		final String fname = name;
+		final JFrame froot = root;
+		final Scene fcurr = curr;
+		final HashMap<String, Scene> fht = ht;
+		
+		addActionListener(new ActionListener() { 			
+			  public void actionPerformed(ActionEvent e) { 
+			    froot.remove(fcurr);
+			    froot.add(fht.get(fname));
+			  } 
+		});
 	}
 }
