8c7a39b730ee493a73e5f628ec4b90f875e0fe14
Amy Wang <greenapple656@gmail.com>
2017-02-18 05:09:37 -0700

    Update Scene.java

--- Scene.java
+++ Scene.java
@@ -1,35 +1,67 @@
 package treehacks;
 
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import acm.graphics.*;
 
-public class Scene {
-	private ArrayList<Link> links;
+public class Scene extends JPanel{
+	
+	//set coords before instantiating
+	private String name;
+	private HashMap<Rectangle, String> coords;
 	private HashMap<Item, Integer> currItems; //not sure exactly how we'll use the
 		//integer, but we'll have to somehow connect adding a specific item with a 
 		//change of state, and this is a possible way
-	private ArrayList<Item> possItems; 
-	private GImage image;
+	private ArrayList<Item> possItems;
+	private Image image;
 	
-	//constructor that sets only the image
-	public Scene(GImage newimage)
+	//"lazy" constructor
+	public Scene(String name, Image image)
 	{
-		image = newimage;
+		this.name = name;
+		this.image = image;
 	}
 	
-	public GImage getImage(){return image;}
-	public void setImage(GImage image){this.image = image;}
+	//constructor
+	public Scene(String name, Image image, JFrame root, HashMap<String, Scene> ht)
+	{
+		this.name = name;
+		this.image = image;
+		setLayout(null);
+		
+		for(Rectangle rect:coords.keySet())
+		{
+			add(new Link(name, ht, root, this, rect));
+		}
+	}
+	
+	protected void paintComponent(Graphics g) {
+	    super.paintComponent(g);
+	    g.drawImage(image, 0, 0, null);
+	}
+	
+	public String getName(){return name;}
+	public void setName(String name){this.name = name;}
+	
+	public void setImage(Image image)
+	{
+		this.image = image;
+		repaint();
+	}
 	
 	//keeping these as ArrayLists, and including the addLink method because
 	//not sure how we're going to deal with the data yet - if we'll add
 	//it all at once or not
-	public ArrayList<Link> getLinks(){return links;}
-	public void setLinks(ArrayList<Link> links){this.links = links;}
-	public void addLink(Link link){links.add(link);}
-	public void addLink(Scene scene, boolean bool, Rectangle hotspot)
-	{links.add(new Link(scene,bool,hotspot));}
+	public HashMap<Rectangle,String> getCoords(){return coords;}
+	public void setCoords(HashMap<Rectangle,String> coords){this.coords = coords;}
+	public void addCoord(Rectangle rect, String name){coords.put(rect, name);}
 
 	public HashMap<Item, Integer> getCurrItems() {return currItems;}
 	public void setCurrItems(HashMap<Item, Integer> currItems) 
