4695e5982166baa7259c2b31e724799d182b84f8
ChloeLeblanc <chloel@stanford.edu>
2017-02-17 23:37:41 -0800

    Add files via upload

---
+++ TreeHacks.java
@@ -0,0 +1,21 @@
+/*
+ * File: TreeHacks.java
+ * -------------------
+ * 
+ */ 
+
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import java.awt.image.*;
+
+import com.sun.medialib.mlib.Image;
+
+public class TreeHacks extends GraphicsProgram {
+	
+}
