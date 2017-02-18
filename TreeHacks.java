7d08fa502ad833375645eb4b54a9a1ca0aeb694c
ChloeLeblanc <chloel@stanford.edu>
2017-02-18 00:00:15 -0800

    Update TreeHacks.java

--- TreeHacks.java
+++ TreeHacks.java
@@ -4,10 +4,6 @@
  * 
  */ 
 
-import acm.graphics.*;
-import acm.program.*;
-import acm.util.*;
-
 import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -17,5 +13,25 @@ import java.awt.image.*;
 import com.sun.medialib.mlib.Image;
 
 public class TreeHacks extends GraphicsProgram {
+	private void run() {
+		addImage1();
+		addMouseListeners();		
+		setUp();
+		getObjects();
+		openDoor();
+	}
+
 	
+	private void addImage1() {
+		JImage image1 = new JImage("//image to be imported");
+		image1.setSize(getWidth(); getHeight());
+		image1.sendToBack();
+		add(image1);		
+		
+	}
+	
+	//The  to mouse being clicked
+	private void MouseClicked(MouseEvent e) {
+		
+	}
 }
