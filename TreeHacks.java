b2dd5ddda6fc2333cd6bf6b3c936f3c3f4637ff8
Amy Wang <greenapple656@gmail.com>
2017-02-18 17:37:59 -0700

    Update TreeHacks.java

--- TreeHacks.java
+++ TreeHacks.java
@@ -1,3 +1,5 @@
+package treehacks;
+
 /*
  * File: TreeHacks.java
  * -------------------
@@ -9,157 +11,137 @@ import java.awt.*;
 import java.awt.event.*;
 
 import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-import com.sun.medialib.mlib.Image;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-public class TreeHacks extends JFrame {
+public class TreeHacks {
 	
-	private Scene Image1;	
-	private Scene Image2;		
-	private Scene Image3;
-	private Scene Image4;	
-	private Scene Image5;
-	private Scene Image6;	
-	private Scene Image7;	
-	private Scene Image8;	
+	private static JFrame rootFrame;
+	private static ArrayList<Scene> scenes;
+	private static HashMap<String, Scene> ht;
 	
-	public void run() {
+	private final static int WIDTH=1366;
+	private final static int HEIGHT=768;
+	
+	public static void main(String[] args)
+	{
+		rootFrame = new JFrame();
+		initScenes();
+		rootFrame.setSize(WIDTH,HEIGHT);
+		rootFrame.add(scenes.get(0));
 		
-		private HashMap<Rectangle, integer> cords1;
-		private HashMap<Rectangle, integer> cords2;
-		private HashMap<Rectangle, integer> cords3;
-		private HashMap<Rectangle, integer> cords4;
-		private HashMap<Rectangle, integer> cords5;
-		private HashMap<Rectangle, integer> cords6;
-		private HashMap<Rectangle, integer> cords7;
-		private HashMap<Rectangle, integer> cords8;
-		
-		//1_Door image: 
-		//arrows
-		cords1.put(new Rectangle(0,380,75,475), 1); 
-		cords1.put(new Rectangle(1275,380,1333,475), 1); 
-		//Door 
-		cords1.put(new Rectangle(755,286,1016,592), 1); 
-		//Key Pad
-		cords1.put(new Rectangle(1036,351,1101,413), 1); 
-		
-		//2_ Desk:
-		//arrows
-		cords2.put(new Rectangle(0,380,75,475), 2); 
-		cords2.put(new Rectangle(1275,380,1333,475), 2); 
-		//desk
-		cords2.put(new Rectangle(254,423,804,617), 2); 
-		
-		//3_Man:
-		//arrows
-		cords3.put(new Rectangle(0,380,75,475), 3); 
-		cords3.put(new Rectangle(1275,380,1333,475), 3); 
-		//man
-		cords3.put(new Rectangle(458,400,655,685), 3);
-		
-		//4_Bookshelf
-		//arrows
-		cords4.put(new Rectangle(0,380,75,475), 4); 
-		cords4.put(new Rectangle(1275,380,1333,475), 4); 
-		//bookshelf
-		cords4.put(new Rectangle(536,277,778,605), 4); 
-		
-		//5_Door CU
-		//Key pad
-		cords5.put(new Rectangle(951,311,1056,412), 5); 
-		//Door
-		cords5.put(new Rectangle(418,125,906,700), 5);
-		
-		//6_Desk CU
-		//Key pad
-		cords6.put(new Rectangle(951,311,1056,412), 6); 
-		//Drawers, top left
-		cords6.put(new Rectangle(193,428,388,497), 6);
-		//Drawers, middle right
-		cords6.put(new Rectangle(777,517,972,587), 6);
-		//papers
-		cords6.put(new Rectangle(345,327,570,380), 6);
-		
-		//7_Man CU
-		//Key pad
-		cords7.put(new Rectangle(951,311,1056,412), 7); 
-		//shackle
-		cords7.put(new Rectangle(747,388,804,432), 7);
-		
-		//8_Bookshelf CU
-		//Key pad
-		cords8.put(new Rectangle(951,311,1056,412), 8); 
-		//bookshelf
-		cords8.put(new Rectangle(636,371,705,414), 8);
-		
-		
-		
-		
-		try {
-		
-		Map<integer, Scene> ht = new HashMap<integer, Scene>();
-		
-		Image1 = new Scene(1,"1_Door.jpg");
-		ht.put(1, Image1);
-		Image2 = new Scene(2,"2_Desk.jpg");
-		ht.put(2, Image2);
-		Image3 = new Scene(3,"3_Man.jpg");
-		ht.put(3, Image3);
-		Image4 = new Scene(4,"4_Bookshelf.jpg");
-		ht.put(4, Image4);
-		Image4 = new Scene(5,"5_DoorCU.jpg");
-		ht.put(5, Image5);		
-		Image4 = new Scene(6,"6_DeskCU.jpg");
-		ht.put(6, Image6);
-		Image4 = new Scene(7,"7_ManCU.jpg");
-		ht.put(7, Image7);
-		Image4 = new Scene(8,"8_BookshelfCU.jpg");
-		ht.put(8, Image8);		   
-				   
-		addActionListener();		
-		setUp();
-			
-		} catch (IOException ex) {
- 			   ex.printStackTrace();
-		}	
+		rootFrame.setVisible(true);
 	}
-
-	
-	private void setUp() {
+	private static void initScenes()
+	{
+		scenes = new ArrayList<Scene>();
+		ht = new HashMap<String, Scene>();
+		String[] filenames = {"img/1_ Door.jpg","img/2_ Desk.jpg","img/3_ Man.jpg","img/4_ Bookshelf.jpg",
+				"img/5_ Door CU.jpg","img/6_ Desk CU.jpg","img/7_ Man CU.jpg","img/8_ Bookshelf CU.jpg"};
 		
-		makeFrame();
-		makeWelcomeLabel();
+		//"lazily" construct the objects
+		for(int i=0; i<filenames.length; i++)
+		{
+			Image img;
+			try {
+			    img = ImageIO.read(new File(filenames[i]));
+			    String name = "image" + (i+1);
+			    Scene newscene = new Scene(name, img);
+			    scenes.add(newscene);
+			    ht.put(name, newscene);
+			} 
+			catch (IOException e) {
+				e.printStackTrace();
+			}			
+		}
+		 
+		//make HashMap<Rectangle, String> for the link lists of the scenes
+		Rectangle[][] linkAreas = {{new Rectangle(0,380,75,475), //image1 - left
+								new Rectangle(1275,380,1333,475), //right
+								new Rectangle(755,286,1016,592), //door
+								new Rectangle(1036,351,1101,413)}, //keypad
+								//image2: desk
+								{new Rectangle(0,380,75,475), //left
+								new Rectangle(1275,380,1333,475), //right
+								new Rectangle(254,423,804,617)}, //desk
+								//image3: man
+								{new Rectangle(0,380,75,475), //left
+								new Rectangle(1275,380,1333,475), //right
+								new Rectangle(458,400,655,685)}, //man
+								//image4: bookshelf
+								{new Rectangle(0,380,75,475), //left
+								new Rectangle(1275,380,1333,475), //right
+								new Rectangle(536,277,778,605),}, //bookshelf
+								//image5: doorCU
+								{new Rectangle(951,311,1056,412), //keypad
+								new Rectangle(418,125,906,700), //door
+								new Rectangle(624,30,730,100)}, //back
+								//image6: deskCU
+								{new Rectangle(624,30,730,100)}, //back
+								//image7: manCU
+								{new Rectangle(747,388,804,432), //shackle
+								new Rectangle(624,30,730,100)}, //back
+								//image8: bookshelfCU
+								{new Rectangle(624,30,730,100)}}; //back
 		
-	}	
+		String[][] linkNames = {{"image2","image4","image5","image5"},
+								{"image3","image1","image6"},
+								{"image4","image2","image7"},
+								{"image1","image3","image8"},
+								{"image5","image5","image1"},//door and keypad shouldn't work
+								{"image2"},
+								{"image7","image3"},
+								{"image4"}};
 		
-	private void makeFrame() {
-		JFrame ourFrame = new JFrame("image.file");
-		add(Image1);		
-	}	
-			
-	private void makeButton () {
+		HashMap<Rectangle, String>[] linkMaps = (HashMap<Rectangle, String>[]) new HashMap[8];
+		for(int i=0; i<linkAreas.length; i++)
+		{
+			HashMap<Rectangle, String> linkMap = new HashMap<Rectangle, String>();
+			for(int j=0; j<linkAreas[i].length; j++)
+			{
+				linkMap.put(linkAreas[i][j], linkNames[i][j]);
+			}
+			linkMaps[i] = linkMap;
+		}
 		
-		panel.setLayout null; 
-		Button b = new Button("");		
-		b.setOpaque(false);
-		b.setContentAreaFilled(false);
-		b.setBorderPainter(false);
-		panel.add(b);
-	
-	}
-	
-	 private void makeWelcomeLabel() {
-		JLabel welcome = new JLabel("Good morning, your task in this game will be to figure out where you are! How? Consider exploring around this room...");
-		welcome.setFont("Calibri-bold-30");
-		welcome.setColor(Color.CYAN); 
-		ourFrame.add(welcome, ourFrame.getWidth()/2, ourFrame.getHeight()/2);		
+		//make ArrayList<Item> for item lists of scenes
+		Rectangle[][] itemAreas = {{},{},{},{},{}, //images 1-5
+									//image6
+									{new Rectangle(193,428,388,497), //book
+										new Rectangle(777,517,972,587), //key
+										new Rectangle(345,327,570,380)}, //papers
+									{}, //image7
+									{new Rectangle(636,371,705,414)}}; //image8
+		String[][] itemFilenames = {{},{},{},{},{},//images 1-5				
+									{"img/books.png","img/key.png","img/papers.png"},
+									{}, //image7
+									{"img/books.png"}};
 		
-	}
-	
-	//The  to mouse being clicked
-	public void actionPerformed (ActionEvent e) {
+		ArrayList<Item> items = new ArrayList<Item>();
+		for(int i=0; i<itemAreas.length; i++)
+		{
+			for(int j=0; j<itemAreas[i].length; j++)
+			{
+				Image img;
+				try {
+				    img = ImageIO.read(new File(itemFilenames[i][j]));
+				    items.add(new Item(itemAreas[i][j],img));
+				} 
+				catch (IOException e) {
+					e.printStackTrace();
+				}		
+			}
+		}
 		
+		for(int i=0; i<scenes.size(); i++)
+		{
+			scenes.get(i).addButtons(rootFrame, ht, linkMaps[i], items);
+		}
 	}
 }
-
-
