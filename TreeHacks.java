3000d55213fb85caf02105e9e442f3e8882b47b3
Amy Wang <greenapple656@gmail.com>
2017-02-19 02:38:20 -0700

    Update TreeHacks.java

--- TreeHacks.java
+++ TreeHacks.java
@@ -17,49 +17,79 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.WindowConstants;
+import javax.swing.border.Border;
 
 public class TreeHacks {
 	
 	private static JFrame rootFrame;
+	private static JLabel label;
 	private static ArrayList<Scene> scenes;
 	private static HashMap<String, Scene> ht;
+	private static Inventory ivt;
 	
+	//public variables suck...but come on, this is *way* more elegant than any other
+	//solution you can think of
+	public static ArrayList<Item> allItems;
 	public final static int WIDTH=1366;
 	public final static int HEIGHT=768;
 	
 	public static void main(String[] args)
 	{
 		rootFrame = new JFrame();
+		label = new JLabel();
+		ivt = new Inventory();
+		allItems = new ArrayList<Item>();
+		
+		rootFrame.getContentPane().add(label);
+		label.setSize(1250,30);
+		label.setFont(new Font(label.getFont().getName(), Font.PLAIN, 25));
+		label.setForeground(Color.RED);
+		label.setVisible(false);
+		label.setLocation(40,30);
+		rootFrame.getContentPane().add(ivt);
+		ivt.setSize(1366,200);
+		ivt.setLocation(Inventory.MIN_X,Inventory.MIN_Y);
+		
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		toolkit.addAWTEventListener(new AWTEventListener() {
+            public void eventDispatched(AWTEvent e) {
+            	if (MouseEvent.MOUSE_CLICKED == e.getID()) {
+                    MouseEvent event = (MouseEvent) e;
+                    hideText();
+                }
+            }
+           },
+        AWTEvent.MOUSE_MOTION_EVENT_MASK + AWTEvent.MOUSE_EVENT_MASK + AWTEvent.KEY_EVENT_MASK);
+		
 		initScenes();
 		rootFrame.setSize(WIDTH,HEIGHT);
 		
-		//JLayeredPane layeredPane = new JLayeredPane();
-		//layeredPane.setLayout(null);
-		
-		//JPanel scrollPanel = new JPanel();
-		//JScrollPane scrollPane = new JScrollPane();
-		//scrollPane.add(scrollPanel);
-		//rootFrame.getContentPane().setLayout(null);
-		//scrollPane.setSize(WIDTH, HEIGHT);
-		//scrollPane.add(scenes.get(0));
-		//rootFrame.setContentPane(layeredPane);
 		scenes.get(0).setSize(WIDTH,HEIGHT);
-		scenes.get(0).setLocation(0,0);
+		scenes.get(0).setLocation(0,0);	
 		rootFrame.getContentPane().add(scenes.get(0));
 		
-		//Inventory ivt = new Inventory();
-		//rootFrame.getContentPane().add(ivt);
-		//ivt.setSize(Inventory.MIN_WIDTH, Inventory.MIN_HEIGHT);
-		//ivt.setLocation(400,400);		
-		
-		rootFrame.setVisible(true);
 		rootFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		rootFrame.setVisible(true);	    
+	    rootFrame.repaint();
+	    
+	    showText("Intro text here");
+	}
+	
+	public static void showText(String text)
+	{
+		label.setText("\t"+text);
+		label.setVisible(true);
+	}
+	public static void hideText()
+	{
+		label.setVisible(false);
 	}
 	
 	private static void initScenes()
@@ -109,8 +139,7 @@ public class TreeHacks {
 								//image6: deskCU
 								{new Rectangle(624,30,106,70)}, //back
 								//image7: manCU
-								{new Rectangle(747,388,57,44), //shackle
-								new Rectangle(624,30,106,70)}, //back
+								{new Rectangle(624,30,106,70)}, //back
 								//image8: bookshelfCU
 								{new Rectangle(624,30,106,70)}}; //back
 		
@@ -120,7 +149,7 @@ public class TreeHacks {
 								{"image1","image3","image8"},
 								{"image5","image5","image1"},//door and keypad shouldn't work
 								{"image2"},
-								{"image7","image3"},
+								{"image3"},
 								{"image4"}};
 		
 		HashMap<Rectangle, String>[] linkMaps = (HashMap<Rectangle, String>[]) new HashMap[8];
@@ -137,13 +166,12 @@ public class TreeHacks {
 		//make ArrayList<Item> for item lists of scenes
 		Rectangle[][] itemAreas = {{},{},{},{},{}, //images 1-5
 									//image6
-									{new Rectangle(193,428,195,69), //book
-										new Rectangle(777,517,195,70), //key
-										new Rectangle(345,327,225,53)}, //papers
+									{new Rectangle(193,428,195,69), //papers
+										new Rectangle(777,517,195,70)}, //key
 									{}, //image7
 									{new Rectangle(636,371,69,43)}}; //image8
 		String[][] itemFilenames = {{},{},{},{},{},//images 1-5				
-									{"img/books.png","img/key.png","img/papers.png"},
+									{"img/papers.png","img/key.png"},
 									{}, //image7
 									{"img/books.png"}};
 		
@@ -155,8 +183,11 @@ public class TreeHacks {
 			{
 				Image img;
 				try {
-				    img = ImageIO.read(new File(itemFilenames[i][j]));
-				    map.put(itemAreas[i][j], new Item(img));
+					String filename = itemFilenames[i][j];
+				    img = ImageIO.read(new File(filename));
+				    Item item = new Item(img,filename.substring(4,filename.length()-4));
+				    map.put(itemAreas[i][j], item);
+				    allItems.add(item);
 				} 
 				catch (IOException e) {
 					e.printStackTrace();
@@ -165,9 +196,45 @@ public class TreeHacks {
 			items[i] = map;
 		}
 		
+		//make HashMap<Rectangle, String> for the link lists of the scenes
+		Rectangle[][] npcAreas = {{},{},{},{},{},{},
+									{new Rectangle(747,388,57,44)}, //back
+									{}}; //back
+		Reception[][] npcFunctions = {{},{},{},{},{},{},
+				{new Reception()
+					{
+						public void react(NPC r)
+						{
+							if(ivt.getActive()==null)
+							{TreeHacks.showText("fr(The key)");}
+							else
+							{
+								if(ivt.getActive().getName().equals("key"))
+								{TreeHacks.showText("Animation happens here.");
+								//here, scene changes would occur
+								//and the scene would get a new link, to outside
+								}
+								else
+								{TreeHacks.showText("fr(no, I need the key)");}
+							}
+						}
+					}}, //back
+				{}}; //back
+		HashMap<Rectangle, Reception>[] NPCs = (HashMap<Rectangle, Reception>[]) new HashMap[npcAreas.length];
+		for(int i=0; i<npcAreas.length; i++)
+		{
+			HashMap<Rectangle, Reception> map = new HashMap<Rectangle, Reception>();
+			for(int j=0; j<npcAreas[i].length; j++)
+			{
+				map.put(npcAreas[i][j], npcFunctions[i][j]);
+			}
+			NPCs[i] = map;
+		}
+		
 		for(int i=0; i<scenes.size(); i++)
 		{
-			scenes.get(i).addButtons(rootFrame, ht, linkMaps[i], items[i]);
+			scenes.get(i).addButtons(rootFrame, ivt, ht, linkMaps[i], items[i], 
+					NPCs[i]);
 		}
 
 	}
